772a05d8129b9c84480ec4f6d81ad86485e5eeb2
Damjan Mozetič <rhuantavan@gmail.com>
2026-03-03 11:59:21 +0100

    assets path edit

--- src/assets/Assets.ts
+++ src/assets/Assets.ts
@@ -1,20 +1,20 @@
 export const assets = {
 	symbols: {
-		bar:	'/assets/images/symbols/Bar.png',
-		bell:	'/assets/images/symbols/Bell.png',
-		cherry:	'/assets/images/symbols/Cherry.png',
-		diamond:'/assets/images/symbols/Diamond.png',
-		lemon:  '/assets/images/symbols/Lemon.png',
-		plum:	'/assets/images/symbols/Plum.png',
-		seven:	'/assets/images/symbols/Seven.png',
-		wild:	'/assets/images/symbols/Wild.png',
+		bar:	'assets/images/symbols/Bar.png',
+		bell:	'assets/images/symbols/Bell.png',
+		cherry:	'assets/images/symbols/Cherry.png',
+		diamond:'assets/images/symbols/Diamond.png',
+		lemon:  'assets/images/symbols/Lemon.png',
+		plum:	'assets/images/symbols/Plum.png',
+		seven:	'assets/images/symbols/Seven.png',
+		wild:	'assets/images/symbols/Wild.png',
 	},
 	ui: {
-		reelFrame:	'/assets/images/ui/ReelFrame.png',
-		separator:	'/assets/images/ui/ReelSeparator.png',
-		spinButton:	'/assets/images/ui/SpinButton.png',
-		winField:	'/assets/images/ui/WinField.png',
-		betField:	'/assets/images/ui/BetField.png',
+		reelFrame:	'assets/images/ui/ReelFrame.png',
+		separator:	'assets/images/ui/ReelSeparator.png',
+		spinButton:	'assets/images/ui/SpinButton.png',
+		winField:	'assets/images/ui/WinField.png',
+		betField:	'assets/images/ui/BetField.png',
 	},
 } as const;
 
